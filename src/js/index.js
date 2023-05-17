729259199cba39bdbae14f86908d9adf33cd19c3
Kenji Cardozo <KenjiCardozo10@gmail.com>
2023-05-16 23:13:06 -0300

    estrutura basica de uma landing page

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
+
+function escorderImagem() {
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+    });
+};
+
+function mostrarImagem() {
+    imagensPainel[imagemAtual].classList.add('mostrar');
+}
+
+setaAvancar.addEventListener('click', function () {
+
+    const totalDeImagens = imagensPainel.length - 1;
+    if (imagemAtual === totalDeImagens) {
+        console.log('não pode avançar mais');
+        return;
+    }
+
+    imagemAtual++;
+
+    escorderImagem();
+    mostrarImagem();
+});
+
+setaVoltar.addEventListener('click', function () {
+
+    if (imagemAtual === 0) {
+        console.log('não tem mais como voltar');
+        return;
+    }
+
+    imagemAtual--;
+
+    escorderImagem();
+    mostrarImagem();
+});
